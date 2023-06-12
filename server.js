c3253d3ed38b5d6323e5d2c76c5f2fde126973ea
Chase Van Halen <cvanhalen@perscholas.org>
2023-06-12 13:55:20 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/dist")));
+
+app.listen(4001, () => {
+    console.log('listening on port ', 4001);
+});
+
